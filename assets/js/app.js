acef3949a2ab168ed47fb5b343e50a906af48183
taingk <taing_k@etna-alternance.net>
2018-12-19 15:18:47 +0100

    add require images recursively

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,12 @@
-// any CSS you require will output into a single css file (app.css in this case)
+const $ = require('jquery');
+
+// CSS
 import 'bootstrap/scss/bootstrap.scss';
 require('../css/app.css');
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-const $ = require('jquery');
+// JS
 require('bootstrap');
+
+// Images
+const imagesContext = require.context('../images', true, /\.(png|jpg|jpeg|gif|ico|svg|webp)$/);
+imagesContext.keys().forEach(imagesContext);
