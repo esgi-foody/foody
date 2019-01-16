b8ce7c5555ee0d267df93c3b04382d1043382a70
Enzo Huynh <enzo_huynh@hotmail.com>
2019-01-16 16:37:01 +0100

    register / login update

---
+++ src/Controller/front/Auth/SecurityController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller\front\Auth;
+
+use App\Form\UserType;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+/**
+ * Class UserController
+ * @package App\Controller
+ * @Route(name="app_front_security_")
+ */
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $helper): Response
+    {
+        return $this->render('front/Security/login.html.twig', [
+            'error' => $helper->getLastAuthenticationError(),
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     * @throws \Exception
+     */
+    public function logout(): void
+    {
+        throw new \Exception('This should never be reached!');
+    }
+
+    /**
+     * @Route("/register", name="registration")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        if ($this->getUser() instanceof User) {
+            return $this->redirectToRoute('home');
+        }
+
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+            $user->setRoles(['ROLE_USER']);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_front_security_login');
+        }
+
+        return $this->render(
+            'front/Security/register.html.twig', [
+                'form' => $form->createView(),
+            ]
+        );
+    }
+}
