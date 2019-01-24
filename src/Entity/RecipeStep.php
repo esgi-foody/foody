73047dbd6e9184b7a735c60cae03b35c8cb2e8a8
Baal-Marduk <leo.wattier@free.fr>
2019-01-24 16:23:18 +0100

    fixtures recipe/recipe_step/ingredient ok

--- src/Entity/RecipeStep.php
+++ src/Entity/RecipeStep.php
@@ -5,14 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use App\Entity\Traits\TimestampableTrait;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\RecipeStepRepository")
  */
 class RecipeStep
 {
-    use TimestampableTrait;
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -46,6 +44,7 @@ class RecipeStep
      * @ORM\OneToMany(targetEntity="App\Entity\Image", mappedBy="recipeStep")
      */
     private $images;
+
 
     public function __construct()
     {
