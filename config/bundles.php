f104fefb3182bea838ea453b23b492ed28fc0850
taingk <taingkn@gmail.com>
2019-01-26 22:23:51 +0100

    add new wysiwyg (#60)
    * add new wysiwyg
    
    * rm .lock

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,6 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -4,16 +4,36 @@ namespace App\Form;
 
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('hexaColor')
+            ->add('name', TextType::class, [
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Prise de masse'
+                ],
+                'constraints' => [
+                    new Length(['min' => 3, 'max' => 30])
+                ],
+                'label' => 'Nom de la catégorie'
+            ])
+            ->add('hexaColor', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                    new Length(['min' => 4, 'max' => 7])
+                ],
+                'label' => 'Code hexadécimal de la couleur',
+                'attr' => [
+                    'placeholder' => '#FFFFFF'
+                ]
+            ])
         ;
     }
 
