5dec8f6e803fc455e0056c8ba1b180856375f69e
Amandine <amandine-ripoll@outlook.fr>
2019-01-27 16:44:05 +0100

    Add input color

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -6,6 +6,7 @@ use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
 
 class CategoryType extends AbstractType
 {
@@ -13,7 +14,7 @@ class CategoryType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('hexaColor')
+            ->add('hexaColor', ColorType::class)
         ;
     }
 
