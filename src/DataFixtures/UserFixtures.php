d8efc52adef917fbb79fd043bc847cfa5fd111d4
Leo Wattier <leo.wattier@free.fr>
2019-01-29 15:26:26 +0100

    Fix/fixture img (#72)
    * food photos on recipes now!
    
    * people photos on user fixture
    
    * Fixed categories and password on fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -13,19 +13,32 @@ class UserFixtures extends Fixture
     {
         $faker = Faker\Factory::create();
 
-        for ($i = 0; $i < 20; $i++) {
+        for ($i = 0; $i < 30; $i++) {
             $user = new User();
             $user->setUsername($faker->userName);
             $user->setPseudo($faker->firstName . " " . $faker->lastName);
-            $user->setPassword($faker->password);
+            $user->setPassword('Foody2019');
             $user->setDateOfBirth($faker->dateTime);
-            $user->setPathImg($faker->imageUrl(640, 480));
+            $user->setPathImg($faker->imageUrl( 480, 480 , 'people'));
             $user->setEmail($faker->email);
             $user->setStatus(rand(0,1));
             $user->setRoles(['ROLE_USER']);
             $manager->persist($user);
 
-            $manager->flush();
+
         }
+
+        $user = new User();
+        $user->setUsername('chloe75');
+        $user->setPseudo('Chloe Fit');
+        $user->setPassword('Foody2019');
+        $user->setDateOfBirth($faker->dateTime);
+        $user->setPathImg($faker->imageUrl( 480, 480 , 'people'));
+        $user->setEmail('chloe.fit-girl@gmail.com');
+        $user->setStatus('1');
+        $user->setRoles(['ROLE_USER']);
+        $manager->persist($user);
+
+        $manager->flush();
     }
 }
