cc0ab4db6c36c18d30b5284a1494b4d84d0b90a1
dycor <dylan.correia@hotmail.com>
2019-01-31 22:10:02 +0100

    file uploader service (#76)

---
+++ src/Service/FileUploader.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class FileUploader
+{
+
+    private $targetDirectory;
+
+    public function __construct()
+    {
+        $this->targetDirectory = 'uploads/';
+    }
+
+    public function upload(UploadedFile $file, $directory )
+    {
+        $fileName = md5(uniqid()).'.'.$file->guessExtension();
+
+        try {
+            $file->move( $this->targetDirectory.$directory, $fileName);
+        } catch (FileException $e) {
+            $this->logger->error("Erreur lors de l'import d'image: " . $e->getMessage());
+            throw new FileException("Erreur import d'image");
+        }
+
+        return $fileName;
+    }
+
+}
