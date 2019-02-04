35b8c0b371c310d452549145f212b29a23c76006
Amandine <amandine-ripoll@outlook.fr>
2019-02-04 11:55:38 +0100

    Edit category and update profile

---
+++ src/Form/ProfileType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => "Nom d'utilisateur"
+            ])
+            ->add('biography', TextType::class, [
+                'label' => "Biographie"
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
