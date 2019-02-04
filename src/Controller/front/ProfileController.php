5572fa0f573f054ccb1e7af0500fb1efb603d4f5
Amandine <amandine-ripoll@outlook.fr>
2019-02-04 11:55:38 +0100

    Edit category and update profile

--- src/Controller/front/ProfileController.php
+++ src/Controller/front/ProfileController.php
@@ -3,10 +3,12 @@
 namespace App\Controller\front;
 
 use App\Entity\User;
+use App\Form\ProfileType;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/profile", name="app_front_")
@@ -21,5 +23,40 @@ class ProfileController extends AbstractController
     public function show(User $user): Response
     {
         return $this->render('front/profile/index.html.twig', ['user' => $user]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="profile_edit", methods="GET|POST")
+     */
+
+    public function edit(Request $request, User $user): Response
+    {
+        $form = $this->createForm(ProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('app_front_profile_show', ['username' => $user->getUsername()]);
+        }
+
+        return $this->render('front/profile/edit.html.twig', [
+            'category' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="profile_delete", methods="DELETE")
+     */
+    public function delete(Request $request, User $user): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($user);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('home');
     }
 }
