0386d3402440d2fdb91eac010c14caf7a0ed3a45
Baal-Marduk <leo.wattier@free.fr>
2019-02-04 10:47:09 +0100

    Compte par défaut root:Foody2019 / chloe:Chloe2019

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -13,30 +13,40 @@ class UserFixtures extends Fixture
     {
         $faker = Faker\Factory::create();
 
-        for ($i = 0; $i < 30; $i++) {
+        for ($i = 0; $i < 20; $i++) {
             $user = new User();
-            $user->setUsername($faker->userName);
-            $user->setPseudo($faker->firstName . " " . $faker->lastName);
-            $user->setPassword('Foody2019');
+            $user->setUsername($faker->firstName . " " . $faker->lastName);
+            $user->setPseudo($faker->userName);
+            $user->setPassword($faker->password);
             $user->setDateOfBirth($faker->dateTime);
-            $user->setPathImg($faker->imageUrl( 480, 480 , 'people'));
+            $user->setPathImg($faker->imageUrl(640, 480));
             $user->setEmail($faker->email);
             $user->setStatus(rand(0,1));
             $user->setRoles(['ROLE_USER']);
             $manager->persist($user);
-
-
         }
 
         $user = new User();
-        $user->setUsername('chloe75');
-        $user->setPseudo('Chloe Fit');
-        $user->setPassword('Foody2019');
+        $user->setUsername('chloe');
+        $user->setPseudo('Chloe fit');
+        $user->setPassword('$2y$13$13d5PNpmneww1yehhoSUweJfVdDRTGQqnjbi8Vxs9d1WY3AloZRFW');
         $user->setDateOfBirth($faker->dateTime);
-        $user->setPathImg($faker->imageUrl( 480, 480 , 'people'));
-        $user->setEmail('chloe.fit-girl@gmail.com');
-        $user->setStatus('1');
+        $user->setPathImg($faker->imageUrl(640, 480));
+        $user->setEmail('chloe@gmail.com');
+        $user->setStatus(1);
         $user->setRoles(['ROLE_USER']);
+        $manager->persist($user);
+
+
+        $user = new User();
+        $user->setUsername('root');
+        $user->setPseudo('admin foody');
+        $user->setPassword('$2y$13$dXOe6srZN/F1eMItPegMgezvWHVOkNVY67goEGncTVXE6YFf2EvCu');
+        $user->setDateOfBirth($faker->dateTime);
+        $user->setPathImg($faker->imageUrl(640, 480));
+        $user->setEmail('root@gmail.com');
+        $user->setStatus(1);
+        $user->setRoles(['ROLE_ADMIN']);
         $manager->persist($user);
 
         $manager->flush();
