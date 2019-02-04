a8b8be6690aef5799946c0dbb2f238e3090193fa
Baal-Marduk <leo.wattier@free.fr>
2019-02-04 10:47:09 +0100

    Compte par défaut root:Foody2019 / chloe:Chloe2019

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -24,8 +24,31 @@ class UserFixtures extends Fixture
             $user->setStatus(rand(0,1));
             $user->setRoles(['ROLE_USER']);
             $manager->persist($user);
-
-            $manager->flush();
         }
+
+        $user = new User();
+        $user->setUsername('chloe');
+        $user->setPseudo('Chloe fit');
+        $user->setPassword('$2y$13$13d5PNpmneww1yehhoSUweJfVdDRTGQqnjbi8Vxs9d1WY3AloZRFW');
+        $user->setDateOfBirth($faker->dateTime);
+        $user->setPathImg($faker->imageUrl(640, 480));
+        $user->setEmail('chloe@gmail.com');
+        $user->setStatus(1);
+        $user->setRoles(['ROLE_USER']);
+        $manager->persist($user);
+
+
+        $user = new User();
+        $user->setUsername('root');
+        $user->setPseudo('admin foody');
+        $user->setPassword('$2y$13$dXOe6srZN/F1eMItPegMgezvWHVOkNVY67goEGncTVXE6YFf2EvCu');
+        $user->setDateOfBirth($faker->dateTime);
+        $user->setPathImg($faker->imageUrl(640, 480));
+        $user->setEmail('root@gmail.com');
+        $user->setStatus(1);
+        $user->setRoles(['ROLE_ADMIN']);
+        $manager->persist($user);
+
+        $manager->flush();
     }
 }
