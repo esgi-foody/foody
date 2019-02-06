d662a6c501522669a5c5cde0020c59ebb3386632
Enzo Huynh <enzo_huynh@hotmail.com>
2019-02-06 15:04:44 +0100

    Refactor Security to auth

--- src/Controller/front/ProfileController.php
+++ src/Controller/front/ProfileController.php
@@ -31,37 +31,4 @@ class ProfileController extends AbstractController
     {
         return $this->render('front/profile/index.html.twig', ['user' => $user]);
     }
-
-    public function resetPassword(Request $request, User $user)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $user = $this->getUser();
-        $form = $this->createForm(ResetPasswordType::class, $user);
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $passwordEncoder = $this->get('security.password_encoder');
-            $oldPassword = $request->request->get('reset_password')['oldPassword'];
-
-            // Si l'ancien mot de passe est bon
-            if ($passwordEncoder->isPasswordValid($user, $oldPassword)) {
-                $newEncodedPassword = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
-                $user->setPassword($newEncodedPassword);
-
-                $em->persist($user);
-                $em->flush();
-
-                $this->addFlash('notice', 'Votre mot de passe à bien été changé !');
-
-                return $this->redirectToRoute('profile_show');
-            } else {
-                $form->addError(new FormError('Ancien mot de passe incorrect'));
-            }
-        }
-
-        return $this->render('front/profile/resetPassword.html.twig', array(
-            'form' => $form->createView(),
-        ));
-    }
 }
