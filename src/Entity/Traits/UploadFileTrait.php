9b0ff6444a8d40177a24f2ed121fe84ee0352c02
Correia Lopes Dylan <dylan.correia@hotmail.com>
2019-02-06 17:48:17 +0100

    Installation bundle VichUploaderBundle & suppression du service uploader
    Installation bundle VichUploaderBundle & suppression du service uploader

---
+++ src/Entity/Traits/UploadFileTrait.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Entity\Traits;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+trait UploadFileTrait
+{
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @var string
+     */
+    private $imageName;
+
+
+    /**
+     * @param null|File $imageFile
+     * @throws \Exception
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+}
