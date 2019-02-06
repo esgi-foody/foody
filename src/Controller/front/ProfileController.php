1f96f4b5fe629c0450c4604ea09efa4475c11458
Baal-Marduk <leo.wattier@free.fr>
2019-02-06 15:58:24 +0100

    conflict resolving

--- src/Controller/front/ProfileController.php
+++ src/Controller/front/ProfileController.php
@@ -5,14 +5,10 @@ namespace App\Controller\front;
 use App\Entity\User;
 use App\Entity\Relationship;
 use App\Repository\UserRepository;
-use App\Repository\RelationshipRepository;
 use phpDocumentor\Reflection\Types\Boolean;
 use phpDocumentor\Reflection\Types\Void_;
-use PhpParser\Node\Scalar\String_;
-use Symfony\Component\DependencyInjection\Compiler\ResolveBindingsPass;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authentication\Provider\RememberMeAuthenticationProvider;
 
@@ -31,9 +27,9 @@ class ProfileController extends AbstractController
     }
 
     /**
-     * @Route("/{username}/follow", name="profile_follow", methods="GET|POST")
+     * @Route("/{username}/follow", name="profile_follow", methods="GET")
      */
-    public function follow(User $user): Response
+    public function follow(User $user): Void_
     {
 
         $relation = new Relationship();
@@ -44,47 +40,14 @@ class ProfileController extends AbstractController
         $em->persist($relation);
         $em->flush();
 
-        return $this->redirectToRoute('app_front_profile_show',['username'=> $user->getUsername()]);
+        return $this->redirectToRoute('app_front_profile_follow');
     }
 
     /**
      * @Route("/{username}/unfollow", name="user_unfollow", methods="GET|POST")
      */
-    public function unfollow(User $user): Response
+    public function unfollow(): Response
     {
-        $em = $this->getDoctrine()->getManager();
-        $follower = $this->getUser();
 
-//        $relation = $this->getDoctrine()->getRepository(Relationship::class)->
-//        findBy(array('followed_id' => $user->getId(), 'follower_id' => $follower->getId()));
-
-        $relation = $this->getDoctrine()->getRepository(Relationship::class)->findOneById($follower->getId(),$user->getId());
-        dump($relation);die();
-        $em->remove($relation);
-        $em->flush();
-
-        return $this->redirectToRoute('app_front_profile_show',['username'=> $user->getUsername()]);
-    }
-
-    /**
-     * @Route("/{username}/isfollower", name="isfollower", methods="GET|POST")
-     */
-    public function isFollower(User $user): Response
-    {
-        $em = $this->getDoctrine()->getManager();
-        $loggedUser = $em->getRepository(User::class)->find($this->getUser()->getId());
-        $loggedUser->getFolloweds();
-        $loggedUser->getFollowers();
-        $this->getDoctrine()->getManager()->initializeObject($loggedUser);
-//        $response = new Response();
-
-
-//        if ( in_array($user,$followed)){
-//            $response->setContent('TRUE');
-//        } else {
-//            $response->setContent('FALSE');
-//        }
-        dump($loggedUser);die();
-        return $reponse;
     }
 }
