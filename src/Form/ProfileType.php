4e3e7d1c0a3fc38fc7e5d1b61f2d1dd9e280af34
amandineripoll <32871125+amandineripoll@users.noreply.github.com>
2019-02-07 10:29:14 +0100

    Edit category and update profile (#83)
    * Edit category and update profile
    
    * Edit category and update profile
    
    * wip delete
    
    * Add recipes in profile, delete only our own profile, update only our own profile, voter created
    
    * Update profile

---
+++ src/Form/ProfileType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Validator\Constraints\Length;
+
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('pseudo', TextType::class, [
+                'label' => "Pseudo",
+                'constraints' => [new Length(['min' => 3, 'max' => 30])]
+            ])
+
+            ->add('biography',CKEditorType::class,[
+                'label' => "Biographie"
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
