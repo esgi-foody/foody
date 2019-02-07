75efe3777ca84acd63e906dcfd7b4df148194abb
dycor <dylan.correia@hotmail.com>
2019-02-07 10:19:07 +0100

    Installation bundle VichUploaderBundle & suppression du service uploader (#87)
    Installation bundle VichUploaderBundle & suppression du service uploader

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
