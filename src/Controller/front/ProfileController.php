2230a8d4c33feb7e7781278d11e23348e04d6ee6
amandineripoll <32871125+amandineripoll@users.noreply.github.com>
2019-02-07 10:29:14 +0100

    Edit category and update profile (#83)
    * Edit category and update profile
    
    * Edit category and update profile
    
    * wip delete
    
    * Add recipes in profile, delete only our own profile, update only our own profile, voter created
    
    * Update profile

--- src/Controller/front/ProfileController.php
+++ src/Controller/front/ProfileController.php
@@ -3,9 +3,14 @@
 namespace App\Controller\front;
 
 use App\Entity\User;
+use App\Form\ProfileType;
+use App\Repository\UserRepository;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 
 /**
@@ -22,4 +27,45 @@ class ProfileController extends AbstractController
     {
         return $this->render('front/profile/index.html.twig', ['user' => $user]);
     }
+
+    /**
+     * @Route("/{id}/edit", name="profile_edit", methods="GET|POST")
+     */
+
+    public function edit(Request $request, User $user): Response
+    {
+        $this->denyAccessUnlessGranted('edit', $user);
+        $form = $this->createForm(ProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('app_front_profile_show', ['username' => $user->getUsername()]);
+        }
+
+        return $this->render('front/profile/edit.html.twig', [
+            'category' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/", name="profile_delete", methods="DELETE")
+     */
+    public function delete(Request $request, TokenStorageInterface $tokenStorage): Response
+    {
+        if (!$this->isCsrfTokenValid('delete'.$this->getUser()->getId(), $request->request->get('_token'))) {
+            throw new AccessDeniedException();
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($this->getUser());
+        $tokenStorage->setToken(null);
+        $request->getSession()->invalidate();
+        $em->flush();
+
+        return $this->redirectToRoute('app_front_security_login');
+    }
+
 }
