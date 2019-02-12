5ca463dc1c0d1ffa5d30345289a8e5caa60d8c5d
Leo Wattier <leo.wattier@free.fr>
2019-02-12 19:52:01 +0100

    feed + sort ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,36 +14,31 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        if ($this->getUser() != null){
-            $relations = $this->getUser()->getFolloweds();
-            $recipes = [];
 
-            foreach ($relations as $relation){
+        $relations = $this->getUser()->getFolloweds();
+        $recipes = [];
+        foreach ($relations as $relation){
 
-                $userRecipes = $relation->getFollowed()->getRecipes();
+            $userRecipes = $relation->getFollowed()->getRecipes();
 
                 foreach ($userRecipes as $userRecipe){
                     $recipes[] = $userRecipe ;
 
                 }
-            }
-
-            usort($recipes, function($a, $b) {
-                return strtotime($a->getUpdatedAt()->format('Y-m-d H:i:s')) - strtotime($b->getUpdatedAt()->format('Y-m-d H:i:s'));
-            });
-
-
-            if (!$recipes) {
-                throw $this->createNotFoundException(
-                    'Aucune recette trouvée :('
-                );
-            }
-            return $this->render('front/home/index.html.twig', [
-                'controller_name' => 'HomeController', 'recipes' => $recipes
-            ]);
-        } else {
-            return $this->redirectToRoute('app_front_auth_login');
         }
 
+        usort($recipes, function($a, $b) {
+            return strtotime($a->getUpdatedAt()->format('Y-m-d H:i:s')) - strtotime($b->getUpdatedAt()->format('Y-m-d H:i:s'));
+        });
+
+
+        if (!$recipes) {
+            throw $this->createNotFoundException(
+                'Aucune recette trouvée :('
+            );
+        }
+        return $this->render('front/home/index.html.twig', [
+            'controller_name' => 'HomeController', 'recipes' => $recipes
+        ]);
     }
 }
