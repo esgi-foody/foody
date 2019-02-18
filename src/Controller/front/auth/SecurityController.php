f7cd94d44bf6c949d45107a5da95591b29d7a967
EnzoHuynh <32840590+EnzoHuynh@users.noreply.github.com>
2019-02-18 20:58:59 +0100

    Update SecurityController.php

--- src/Controller/front/auth/SecurityController.php
+++ src/Controller/front/auth/SecurityController.php
@@ -13,7 +13,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
