28f8955491906c0c48e595012e35ced9168e19b2
taingk <taingkn@gmail.com>
2019-02-22 18:17:41 +0100

    Calorie OK (#111)
    * wip: integration done
    
    * calorie min max done
    
    * wip: integration done
    
    * calorie min max done
    
    * calorie min max ok
    
    * fix logique

--- src/Form/ExplorerType.php
+++ src/Form/ExplorerType.php
@@ -3,13 +3,22 @@
 namespace App\Form;
 
 use App\Entity\Category;
+use App\Repository\RecipeRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class ExplorerType extends AbstractType
 {
+    private $recipeRepository;
+
+    public function __construct(RecipeRepository $recipeRepository)
+    {
+        $this->recipeRepository = $recipeRepository;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -18,11 +27,33 @@ class ExplorerType extends AbstractType
                 'required' => false,
             ])
             ->add('category', EntityType::class, [
-                'label'        => 'Catégories',
-                'class'        => Category::class,
+                'label' => 'Catégories',
+                'class' => Category::class,
                 'choice_label' => 'name',
-                'multiple'     => true,
-                'required'     => false,
+                'multiple' => true,
+                'required' => false,
+            ])
+            ->add('calorie_min', ChoiceType::class, [
+                'label' => 'Calorie minimum',
+                'required' => false,
+                'choices' => $this->getCalorieChoices(),
+            ])
+            ->add('calorie_max', ChoiceType::class, [
+                'label' => 'Calorie maximum',
+                'required' => false,
+                'choices' => $this->getCalorieChoices(),
             ]);
+    }
+
+    public function getCalorieChoices()
+    {
+        $maxCalories = $this->recipeRepository->findHighestCalorie()[1];
+        $maxChoices = [];
+        for ($i = 0; $i < $maxCalories; $i += 100) {
+            $maxChoices[$i] = $i;
+        }
+        $maxChoices[$maxCalories] = $maxCalories;
+
+        return $maxChoices;
     }
 }
