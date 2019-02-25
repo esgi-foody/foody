745e07c4b8512487eb14cf3087e58f6db3c5b4c3
Amandine <amandine-ripoll@outlook.fr>
2019-02-25 16:13:22 +0100

    change password test

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -10,6 +10,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Validator\Constraints\Length;
 use Vich\UploaderBundle\Form\Type\VichImageType;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 
 class ProfileType extends AbstractType
@@ -34,6 +37,25 @@ class ProfileType extends AbstractType
                 'download_label' => false,
                 'label' => 'Image de profil'
             ])
+
+            ->add('password', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Mot de passe'),
+                'second_options' => array('label' => 'Confirmer le mot de passe'),
+                'invalid_message' => 'Les mots de passes ne correspondent pas',
+                'constraints' => [
+                    new Length([
+                        'min' => 6,
+                        'max' => 20,
+                        'minMessage' => 'Le mot de passe doit contenir entre 6 et 20 caractères',
+                        'maxMessage' => 'Le mot de passe doit contenir entre 6 et 20 caractères',
+                    ]),
+                    new Regex([
+                        'pattern' => '/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/',
+                        'message' => 'Le mot de passe doit contenir au moins 1 caractère majuscule, minuscule, 1 chiffre et peut contenir des symboles'
+                    ])
+                ]
+            ))
         ;
     }
 
